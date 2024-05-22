444c13a9f5f4a31dd273d587101ee76c6ff77b11
Ulthran <ctbushman@gmail.com>
2024-05-22 09:19:16 -0400

    Add sample_registry_version command

--- app/sample_registry/src/sample_registry/__init__.py
+++ app/sample_registry/src/sample_registry/__init__.py
@@ -6,6 +6,9 @@ from sqlalchemy.orm import sessionmaker
 
 __version__ = "0.1.0"
 
+def sample_registry_version():
+    sys.stderr.write(__version__)
+
 try:
     db_host = os.environ["DB_HOST"]
     db_user = os.environ["DB_USER"]
@@ -14,10 +17,10 @@ try:
     SQLALCHEMY_DATABASE_URI = f"postgresql://{db_user}:{db_pswd}@{db_host}/{db_name}"
 except KeyError:
     # For development purposes, use a SQLite db prefilled with some demo data
-    sys.stderr.write(
+    sys.stdout.write(
         "Missing database connection information in environment, using test SQLite database\n"
     )
-    sys.stderr.write(
+    sys.stdout.write(
         f"DB_HOST: {os.environ.get('DB_HOST')}\nDB_USER: {os.environ.get('DB_USER')}\nDB_NAME: {os.environ.get('DB_NAME')}\nDB_PSWD: {os.environ.get('DB_PSWD')}\n"
     )
     SQLALCHEMY_DATABASE_URI = f"sqlite:///{Path(__file__).parent.parent.parent.parent.parent.resolve()}/sample_registry.sqlite3"
