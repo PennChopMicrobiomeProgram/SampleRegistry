8f007bc03425a255fdd7f18cf5e525a20ae04c18
Ulthran <ctbushman@gmail.com>
2024-06-04 08:38:35 -0400

    Rename env vars to be project specific

--- app/sample_registry/src/sample_registry/__init__.py
+++ app/sample_registry/src/sample_registry/__init__.py
@@ -11,17 +11,15 @@ def sample_registry_version():
     sys.stderr.write(__version__)
 
 
-print({k: v for k, v in os.environ.items() if "PYTEST" in k})
-
 if "PYTEST_VERSION" in os.environ:
     # Set SQLALCHEMY_DATABASE_URI to an in-memory SQLite database for testing
     SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
 else:
     try:
-        db_host = os.environ["DB_HOST"]
-        db_user = os.environ["DB_USER"]
-        db_name = os.environ["DB_NAME"]
-        db_pswd = os.environ["DB_PSWD"]
+        db_host = os.environ["SAMPLE_REGISTRY_DB_HOST"]
+        db_user = os.environ["SAMPLE_REGISTRY_DB_USER"]
+        db_name = os.environ["SAMPLE_REGISTRY_DB_NAME"]
+        db_pswd = os.environ["SAMPLE_REGISTRY_DB_PSWD"]
         SQLALCHEMY_DATABASE_URI = (
             f"postgresql://{db_user}:{db_pswd}@{db_host}/{db_name}"
         )
@@ -31,7 +29,7 @@ else:
             "Missing database connection information in environment, using test SQLite database\n"
         )
         sys.stdout.write(
-            f"DB_HOST: {os.environ.get('DB_HOST')}\nDB_USER: {os.environ.get('DB_USER')}\nDB_NAME: {os.environ.get('DB_NAME')}\nDB_PSWD: {os.environ.get('DB_PSWD')}\n"
+            f"SAMPLE_REGISTRY_DB_HOST: {os.environ.get('SAMPLE_REGISTRY_DB_HOST')}\nSAMPLE_REGISTRY_DB_USER: {os.environ.get('SAMPLE_REGISTRY_DB_USER')}\nSAMPLE_REGISTRY_DB_NAME: {os.environ.get('SAMPLE_REGISTRY_DB_NAME')}\nSAMPLE_REGISTRY_DB_PSWD: {os.environ.get('SAMPLE_REGISTRY_DB_PSWD')}\n"
         )
         SQLALCHEMY_DATABASE_URI = f"sqlite:///{Path(__file__).parent.parent.parent.parent.parent.resolve()}/sample_registry.sqlite3"
 
