b2ba39c567a4637b8484519c592c649b6208884a
Ulthran <ctbushman@gmail.com>
2024-10-15 13:36:21 -0400

    Bump version to 1.0.5

--- app/sample_registry/src/sample_registry/__init__.py
+++ app/sample_registry/src/sample_registry/__init__.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-__version__ = "1.0.4"
+__version__ = "1.0.5"
 
 
 def sample_registry_version():
