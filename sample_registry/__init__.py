1f83cb2dfc91ef7508aaabe24fc23d36a171adca
Kyle Bittinger <kylebittinger@gmail.com>
2026-03-28 16:24:57 -0400

    Move stuff out of __init__, load test data if needed

--- sample_registry/__init__.py
+++ sample_registry/__init__.py
@@ -8,36 +8,3 @@ from typing import Optional
 __version__ = "1.4.1"
 
 
-# Define archive root path
-ARCHIVE_ROOT = Path(
-    os.environ.get("SAMPLE_REGISTRY_ARCHIVE_ROOT", "/mnt/isilon/microbiome/")
-)
-# Doesn't include "NA" because that's what we fill in for missing values
-NULL_VALUES: list[Optional[str]] = [
-    None,
-    "",
-    "null",
-    "NULL",
-    "None",
-    "none",
-    "NONE",
-    "N/A",
-    "n/a",
-    "na",
-]
-
-
-def sample_registry_version():
-    sys.stderr.write(__version__)
-
-SQLALCHEMY_DATABASE_URI = os.environ.get("SAMPLE_REGISTRY_DB_URI")
-DATABASE_URI_MISSING = SQLALCHEMY_DATABASE_URI is None
-if DATABASE_URI_MISSING:
-    sys.stdout.write(
-        "SAMPLE_REGISTRY_DB_URI not defined in environment, "
-        "using in-memory SQLite database\n")
-    SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
-
-engine = create_engine(SQLALCHEMY_DATABASE_URI)
-Session = sessionmaker(bind=engine)
-session = Session()
